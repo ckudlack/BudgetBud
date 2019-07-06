69c58fe82f0aeff5204a3e62a13d2e0fef1a542c
ckudlack <chrisk@turo.com>
2019-07-06 13:58:09 -0400

    Added getItemsByDate query method

--- app/src/main/java/com/cdk/budgetbud/room/BudgetItemDAO.kt
+++ app/src/main/java/com/cdk/budgetbud/room/BudgetItemDAO.kt
@@ -15,4 +15,7 @@ interface BudgetItemDAO {
 
     @Query("SELECT * from budget_item_table WHERE name LIKE :id")
     fun getBudgetItem(id: Int): RoomBudgetItem
+
+    @Query("SELECT * from budget_item_table WHERE time BETWEEN :minTime AND :maxTime")
+    fun getItemsByDate(minTime: Long, maxTime: Long): List<RoomBudgetItem>
 }
